78b0fbe0d84102d18105a8bfb0e3315d7e808fcc
Nikoleto Scripts <151961875+nikoladhima@users.noreply.github.com>
2025-12-11 22:52:37 +0200

    Update NikoletoService.lua

--- utils/NikoletoService.lua
+++ utils/NikoletoService.lua
@@ -9,7 +9,7 @@
 ----------------
 
 --[[ How to opt out from Nikoleto Service:
-    1. Use this loadstring instead if you are not already using:
+    1. Use this loadstring instead:
     loadstring(game:HttpGet("https://raw.githubusercontent.com/nikoladhima/Duel-Warriors-Hub/main/DuelWarriorsHub.lua"))("RemoveLogging")
     Thats it you've done it lol, stay secret mr agent 🕵️‍♂️
 ]]
